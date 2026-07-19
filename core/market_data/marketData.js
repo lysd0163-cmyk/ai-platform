4f72e98e80596460199c31195767628dd96e382c
lysd0163-cmyk <lysd0163@gmail.com>
2026-07-19 21:55:42 +0300

    Add market data placeholder service

---
+++ core/market_data/marketData.js
@@ -0,0 +1,10 @@
+async function getMarketData() {
+  return {
+    status: 'placeholder',
+    message: 'Market data engine is under construction.',
+  };
+}
+
+module.exports = {
+  getMarketData,
+};
