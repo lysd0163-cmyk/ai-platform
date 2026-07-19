c22eb214d424ce89a5077d88309440877158cc70
lysd0163-cmyk <lysd0163@gmail.com>
2026-07-19 21:59:13 +0300

    Add strategy rules

---
+++ core/strategy_engine/rules.js
@@ -0,0 +1,13 @@
+const RULES = {
+  mode: 'placeholder',
+  notes: 'Strategy rules will be loaded and validated here.',
+};
+
+function getRules() {
+  return RULES;
+}
+
+module.exports = {
+  RULES,
+  getRules,
+};
