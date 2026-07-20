29615f77c6ff4e59c39b2f6b52df556ef091895d
lysd0163-cmyk <lysd0163@gmail.com>
2026-07-20 10:30:29 +0300

    Expand strategy engine exports

--- core/strategy_engine/index.js
+++ core/strategy_engine/index.js
@@ -1,5 +1,9 @@
 const { parseStrategy } = require('./parser');
+const { compileStrategy } = require('./compiler');
+const { runStrategyPipeline } = require('./pipeline');
 
 module.exports = {
   parseStrategy,
+  compileStrategy,
+  runStrategyPipeline,
 };
