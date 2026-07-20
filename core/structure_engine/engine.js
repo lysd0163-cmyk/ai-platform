4195513911df13dd3456c1e98460da6029ba85bf
lysd0163-cmyk <lysd0163@gmail.com>
2026-07-20 09:57:02 +0300

    Add structure engine

---
+++ core/structure_engine/engine.js
@@ -0,0 +1,11 @@
+function runStructureEngine({ marketSnapshot } = {}) {
+  return {
+    marketSnapshot,
+    status: 'placeholder',
+    structure: null,
+  };
+}
+
+module.exports = {
+  runStructureEngine,
+};
