07e791141f1734a2c67b550ffcd25ee7e6c4e0d8
lysd0163-cmyk <lysd0163@gmail.com>
2026-07-20 16:37:42 +0300

    Export full acquisition toolkit

--- core/data_acquisition/index.js
+++ core/data_acquisition/index.js
@@ -1,5 +1,10 @@
 const { trimCandleWindow, normalizeCandles } = require('./window');
 const { normalizeChartAsset, collectChartEvidence } = require('./charts');
+const { createDataProvider } = require('./provider');
+const { createAcquisitionCache } = require('./cache');
+const { validateCandles, validateChartEvidence, validateAcquisitionSnapshot } = require('./validator');
+const { chooseLatestTimestamp, synchronizeAcquisition } = require('./sync');
+const { withRetry } = require('./retry');
 const { acquirePairTimeframeData, acquireMarketPackage } = require('./engine');
 const { createAcquisitionLoop } = require('./loop');
 
@@ -8,6 +13,14 @@ module.exports = {
   normalizeCandles,
   normalizeChartAsset,
   collectChartEvidence,
+  createDataProvider,
+  createAcquisitionCache,
+  validateCandles,
+  validateChartEvidence,
+  validateAcquisitionSnapshot,
+  chooseLatestTimestamp,
+  synchronizeAcquisition,
+  withRetry,
   acquirePairTimeframeData,
   acquireMarketPackage,
   createAcquisitionLoop,
